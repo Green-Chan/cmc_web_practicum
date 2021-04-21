938726185860da755cebb5e6dde4a0888b08f83a
green-chan <l_karinka00@mail.ru>
2021-04-21 16:47:38 +0300

    get data from db throgh controller

--- src/controller/ServiceTypeController.java
+++ src/controller/ServiceTypeController.java
@@ -5,17 +5,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import dao.implementations.ServiceTypeDAOImpl;
+import dao.interfaces.ServiceTypeDAO;
+
 @Controller
 public class ServiceTypeController {
   // @Autowired
-  // private ServiceTypeDAO serviceTypeDAO;
+  private ServiceTypeDAO serviceTypeDAO = new ServiceTypeDAOImpl();
 
   @RequestMapping(value = "/service_types", method = RequestMethod.GET)
   public ModelAndView serviceTypes() {
     ModelAndView modelAndView = new ModelAndView();
-    modelAndView.addObject("serviceTypes", null);
-    // modelAndView.setViewName("ServiceTypes");
-    // TODO: file /WEB-INF/views/ServiceTypes.jsp
+    modelAndView.addObject("serviceTypes", serviceTypeDAO.findAll());
+    modelAndView.setViewName("serviceTypes");
     return modelAndView;
   }
 
